39259734c96a5cdebfc080ebdade4047e31ebdfd
Andrii Yakovliev <andriiyakovliev@mac-mini-andrii.local>
2016-07-05 22:34:29 +0300

    Initial Commit

---
+++ Texter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Texter
+//
+//  Created by  Andrii Yakovliev on 05.07.16.
+//  Copyright © 2016 Andrii Yakovliev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
